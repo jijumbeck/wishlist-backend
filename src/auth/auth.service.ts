2d53fe9451b640b734fb5c76f3c8cd92fc4ea5ee
sonya <jijumbeck@gmail.com>
2024-04-01 21:53:20 +0300

    fix: set salt rounds to const var

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,19 +7,25 @@ import { Auth } from './auth.model';
 import { LoginCredentials, RegisterCredentials, Tokens } from './auth.dto';
 import { UserService } from 'src/user/user.service';
 import { isEmail } from './auth.schema';
+import { User } from 'src/user/user.model';
+
 
 @Injectable()
 export class AuthService {
+    declare salt: number;
+
     constructor(private userService: UserService,
         @InjectModel(Auth) private authRepository: typeof Auth,
-        private jwtService: JwtService) { }
+        private jwtService: JwtService) {
+        this.salt = Number(process.env.BCRYPT_SECRET_HASH);
+    }
 
     async registerUser(registerCredentials: RegisterCredentials) {
         // Create User
         const user = await this.userService.createUser(registerCredentials);
 
         // Create Auth
-        const passwordHash = await bcrypt.hash(registerCredentials.password, process.env.BCRYPT_SECRET_HASH || 5);
+        const passwordHash = await bcrypt.hash(registerCredentials.password, this.salt);
         const auth = await this.authRepository.create({
             userId: user.id,
             passwordHash: passwordHash
@@ -33,7 +39,7 @@ export class AuthService {
         const isLoginByEmail = isEmail(loginCredentials.emailOrLogin);
 
         // Find User (or exception if does not exist)
-        let user = null;
+        let user: User | null = null;
 
         if (isLoginByEmail) {
             user = await this.userService.getUserByEmail({ email: loginCredentials.emailOrLogin });
@@ -73,7 +79,7 @@ export class AuthService {
             throw new BadRequestException("Пользователь с таким id не существует.");
         }
 
-        const passwordHash = await bcrypt.hash(newPassword, process.env.BCRYPT_SECRET_HASH || 5);
+        const passwordHash = await bcrypt.hash(newPassword, this.salt);
         user.passwordHash = passwordHash;
         user.save();
     }
@@ -95,6 +101,14 @@ export class AuthService {
             refreshToken: this.jwtService.sign(payload, {
                 expiresIn: '30d'
             })
+        }
+    }
+
+    private async hashPassword(password: string) {
+        try {
+            return await bcrypt.hash(password, process.env.BCRYPT_SECRET_HASH || 5);
+        } catch (e) {
+            console.log(e);
         }
     }
 }
